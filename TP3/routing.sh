7575693a47e9f7a359e2ffb5787620269ec7978c
axcm19 <afonsoxcm@gmail.com>
2024-05-07 18:52:28 +0100

    connection fully working

---
+++ TP3/routing.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+sudo sysctl -w net.ipv4.ip_forward=1
+sudo iptables -t nat -A POSTROUTING -o enp0s3 -j MASQUERADE
+sudo systemctl stop ufw
+sudo iptables -I FORWARD 1 -j ACCEPT
